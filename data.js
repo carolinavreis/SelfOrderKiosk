c9c1e615d08c8e39c25e8bc9cc22e4a70eb49330
Carolina Reis <71522035+carolinavreis@users.noreply.github.com>
2021-07-29 14:46:26 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,85 @@
+const data = {
+    categories:[
+        {
+            name:'Beverages', 
+            image: '/images/beverages.jpg'
+        },
+        {
+            name:'Breakfast', 
+            image: '/images/breakfast.jpg'
+        },
+        {
+            name:'Burgers', 
+            image: '/images/burgers.jpg'
+        },
+    ],
+        products: [
+            {
+              category: 'Beverages',
+              name: 'Coca-Cola',
+              calorie: 120,
+              price: 1,
+              image: '/images/cocacola.jpg',
+            },
+            {
+              category: 'Beverages',
+              name: 'Vanill Shake',
+              price: 1.5,
+              calorie: 360,
+              image: '/images/vanilla.jpg',
+            },
+            {
+              category: 'Beverages',
+              name: 'Hot Chocolate',
+              price: 2,
+              calorie: 170,
+              image: '/images/cafe.jpg',
+            },
+            {
+              category: 'Breakfast',
+              name: 'Bacon & Biscuit',
+              price: 1.9,
+              calorie: 90,
+              image:
+                '/images/bacon.jpg',
+            },
+            {
+              category: 'Breakfast',
+              name: 'Blueberry Muffin',
+              price: 1.5,
+              calorie: 120,
+              image: '/images/muffin.jpg',
+            },
+            {
+              category: 'Breakfast',
+              name: 'Big Breakfast',
+              price: 3,
+              calorie: 430,
+              image: '/images/biscuit.jpg',
+            },
+            {
+              category: 'Burgers',
+              name: 'Big Mac',
+              price: 1.9,
+              calorie: 200,
+              image: '/images/bigmac.jpg',
+            },
+            {
+              category: 'Burgers',
+              name: 'Hamburger',
+              price: 1.5,
+              calorie: 410,
+              image: '/images/hamburguer.jpg',
+            },
+            {
+              category: 'Burgers',
+              name: 'McDouble',
+              price: 3,
+              calorie: 320,
+              image: '/images/double.jpg',
+            },
+    ]
+};
+
+module.exports = data;
+
